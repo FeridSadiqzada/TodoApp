f8e9e1885169f051a9dc6c754ef3ef92377020df
Aynur <wirinovaaynur123@gmail.com>
2023-11-21 19:04:00 +0400

    todo

--- src/main/java/org/example/repository/TodoRepository.java
+++ src/main/java/org/example/repository/TodoRepository.java
@@ -6,38 +6,21 @@ import java.util.UUID;
 
 
 
-public class TodoRepository extends JpaRepository<Todo, String>  {
-
-    Todo findByTitle(String title);
-
-    List<Todo> findAllByTitle(String title);
+public class TodoRepository   {
 
     private final List<Todo> todoList = new ArrayList<>();
-
 
     public void addTodo(Todo todo) {
         todoList.add(todo);
     }
 
     public void deleteTodo(UUID id) {
-        if (todo.containsKey(id)) {
-            Todo deletedTask = todo.remove(id);
-            System.out.println("Task deleted: " + deletedTask);
-        } else {
-            System.out.println( " not found. No task deleted.");
-        }
-       }
 
+       }
     public void updateTodo(UUID uuid, Todo todo) {
-        if (todo.containsKey(id)) {
-            todo.put(id, updatedTodo);
-            System.out.println("Task updated: " + updatedTodo);
-        } else {
-            System.out.println(" not found. No task updated.");
-        }
 
     }
-    public List<Todo> readTodo() {
+    public List<Todo> readTasks() {
         return todoList;
     }
 }
--- src/main/java/org/example/service/TodoService.java
+++ src/main/java/org/example/service/TodoService.java
@@ -11,31 +11,6 @@ public class TodoService {
 
     private TodoRepository todoRepository;
 
-    //POST
-    public Todo saveCourse(Todo todo) {
-        System.out.println(todo.toString());
-        return todoRepository.save(todo);
-    }
-
-    //Optional!
-    public List<Todo> saveCourses(List<Todo> todos) {
-        return todoRepository.saveAll(todos);
-    }
-
-    //GET
-    public List<Todo> getTodos() {
-        return todosRepository.findAll();
-    }
-    public Todo getTodoById(UUID id) {
-        return todoRepository.findById(id).orElse(null);
-    }
-    public Todo getTodoByTitle(String title) {
-        return todoRepository.findByName(title);
-    }
-    public List<Todo> getTodoForUser(String status) {
-        return todoRepository.findAllByStatus(status);
-    }
-
     private Scanner scanner = new Scanner(System.in);
     private final TodoRepository repository;
 
@@ -68,17 +43,11 @@ public class TodoService {
     }
 
     public void updateTask(Scanner scanner) {
-        System.out.println("updates");
-        Todo updaterecord = TodoRepository.findById(todo.getId()).orElse(null);
-        updaterecord.setTitle(todo.getName());
-        updaterecord.setDescription(todo.getDescription());
-        updaterecord.setStatus(todo.getStatus());
-        return todoRepository.save(updaterecord);
+
     }
 
     public List<Todo> readTasks(UUID id) {
-        todoRepository.deleteById(id);
-        return id + " id -> record removed/completed";
-//        return repository.readTasks();
+//
+        return repository.readTasks();
     }
 }
